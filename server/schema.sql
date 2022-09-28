92797068ac08858e979276a0961460ad94e90efa
Caleb <calebdunderwood@gmail.com>
2022-09-28 15:59:08 -0500

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,12 +1,55 @@
+DROP DATABASE IF EXISTS chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+
+CREATE TABLE users (
+id int not null primary key AUTO_INCREMENT,
+username varchar(30)
 );
 
-/* Create other tables and define schemas for them here! */
+
+ CREATE TABLE rooms (
+  id int not null primary key AUTO_INCREMENT,
+  roomname varchar(30)
+ );
+
+CREATE TABLE messages (
+  /* Describe your table here.*/
+-- column_name data_type(length) [NOT NULL] [DEFAULT value] [AUTO_INCREMENT] column_constraint;
+  /*
+  id
+  message
+  createdAt
+  room_id
+  user
+  */
+  id int not null primary key AUTO_INCREMENT,
+  message varchar(200),
+  createdAt date not null,
+  room_id int,
+  user_id int,
+
+  FOREIGN KEY (room_id) REFERENCES rooms(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+
+
+);
+
+/* Create other tables and define schemas for them here!
+ROOMs
+ID
+Room
+
+
+users
+ID
+Name
+ */
+
+
 
 
 
